0b0db131fb10d7566c6f4dc6daf407117a5f7a80
Juhani Pirttilahti <juhani.pirttilahti@gmail.com>
2021-03-21 16:33:53 +0200

    Updated railway POI definitions

--- layers/railway_poi/class.sql
+++ layers/railway_poi/class.sql
@@ -2,18 +2,41 @@ CREATE OR REPLACE FUNCTION railway_poi_class_rank(class text)
     RETURNS int AS
 $$
 SELECT CASE class
-           WHEN 'railway' THEN 40
+           WHEN 'station' THEN 20
+           WHEN 'yard' THEN 40
+           WHEN 'museum' THEN 50
+           WHEN 'container_terminal' THEN 60
+           WHEN 'halt' THEN 70
+           WHEN 'service_station' THEN 80
+           WHEN 'junction' THEN 90
+           WHEN 'spur_junction' THEN 100
+           WHEN 'crossover' THEN 110
+           WHEN 'site' THEN 120
+           WHEN 'level_crossing' THEN 130
+           WHEN 'tram_level_crossing' THEN 150
+           WHEN 'crossing' THEN 160
+           WHEN 'tram_crossing' THEN 170
+           WHEN 'border' THEN 180
+           WHEN 'owner_change' THEN 190
+           WHEN 'subway_entrance' THEN 200
+           WHEN 'tram_stop' THEN 210
+           WHEN 'crossing_box' THEN 300
+           WHEN 'signal_box' THEN 310
+           WHEN 'blockpost' THEN 320
+           WHEN 'signal' THEN 330
+           WHEN 'switch' THEN 400
+           WHEN 'derail' THEN 490
+           WHEN 'phone' THEN 500
+           WHEN 'radio' THEN 510
+           WHEN 'defect_detector' THEN 520
+           WHEN 'loading_ramp' THEN 600
+           WHEN 'car_shuttle' THEN 610
+           WHEN 'rolling_highway' THEN 620
+           WHEN 'loading_rack' THEN 630
+           WHEN 'turntable' THEN 640
+           WHEN 'traverser' THEN 650
+           WHEN 'milestone' THEN 700
            ELSE 1000
-           END;
-$$ LANGUAGE SQL IMMUTABLE
-                PARALLEL SAFE;
-
-CREATE OR REPLACE FUNCTION railway_poi_class(subclass text, mapping_key text)
-    RETURNS text AS
-$$
-SELECT CASE
-           %%FIELD_MAPPING: class %%
-           ELSE subclass
            END;
 $$ LANGUAGE SQL IMMUTABLE
                 PARALLEL SAFE;
--- layers/railway_poi/railway_poi.sql
+++ layers/railway_poi/railway_poi.sql
@@ -1,5 +1,5 @@
 -- etldoc: layer_railway_poi[shape=record fillcolor=lightpink, style="rounded,filled",
--- etldoc:     label="layer_railway_poi | <z14_> z14+" ] ;
+-- etldoc:     label="layer_railway_poi | <z8> z8 | <z9> z9 | <z10> z10 | <z11> z11 | <z12> z12 | <z13> z13 | <z14_> z14+" ] ;
 
 CREATE OR REPLACE FUNCTION layer_railway_poi(bbox geometry, zoom_level integer, pixel_width numeric)
     RETURNS TABLE
@@ -11,45 +11,149 @@ CREATE OR REPLACE FUNCTION layer_railway_poi(bbox geometry, zoom_level integer, 
                 name_de  text,
                 tags     hstore,
                 class    text,
-                subclass text,
+                ref      text,
+                position text,
+                uic_ref  text,
                 layer    integer,
                 "rank"   int
             )
 AS
 $$
 SELECT osm_id_hash AS osm_id,
-       geometry,
-       NULLIF(name, '') AS name,
-       COALESCE(NULLIF(name_en, ''), name) AS name_en,
-       COALESCE(NULLIF(name_de, ''), name, name_en) AS name_de,
-       tags,
-       railway_poi_class(subclass, mapping_key) AS class,
-       subclass,
-       NULLIF(layer, 0) AS layer,
-       row_number() OVER (
-           PARTITION BY LabelGrid(geometry, 100 * pixel_width)
-           ORDER BY CASE WHEN name = '' THEN 2000 ELSE railway_poi_class_rank(railway_poi_class(subclass, mapping_key)) END ASC
-           )::int AS "rank"
+    geometry,
+    NULLIF(name, '') AS name,
+    COALESCE(NULLIF(name_en, ''), name) AS name_en,
+    COALESCE(NULLIF(name_de, ''), name, name_en) AS name_de,
+    tags,
+    class,
+    COALESCE(NULLIF(ref, ''), railway_ref) AS ref,
+    COALESCE(NULLIF(railway_position, '', railway_position_exact) AS position,
+    NULLIF(uic_ref, '') AS uic_ref,
+    NULLIF(layer, 0) AS layer,
+    row_number() OVER (
+        PARTITION BY LabelGrid(geometry, 100 * pixel_width)
+        ORDER BY railway_poi_class_rank(railway) ASC
+        )::int AS "rank"
 FROM (
-         -- etldoc: osm_railway_poi_point ->  layer_railway_poi:z14_
-         SELECT *,
-                osm_id * 10 AS osm_id_hash
-         FROM osm_railway_poi_point
-         WHERE geometry && bbox
-           AND zoom_level >= 14
+        -- etldoc: osm_railway_poi_point ->  layer_railway_poi:z8
+        -- etldoc: osm_railway_poi_point ->  layer_railway_poi:z9
+        SELECT *,
+              osm_id * 10 AS osm_id_hash
+        FROM osm_railway_poi_point
+        WHERE geometry && bbox
+          AND zoom_level BETWEEN 8 AND 9
+          AND class IN ('station', 'yard', 'junction', 'spur_junction', 'service_station', 'crossover', 'site')
 
-         UNION ALL
+        UNION ALL
 
-         -- etldoc: osm_railway_poi_polygon ->  layer_railway_poi:z14_
-         SELECT *,
-                CASE
-                    WHEN osm_id < 0 THEN -osm_id * 10 + 4
-                    ELSE osm_id * 10 + 1
-                    END AS osm_id_hash
-         FROM osm_railway_poi_polygon
-         WHERE geometry && bbox
-           AND zoom_level >= 14
-     ) AS railway_poi_union
+        -- etldoc: osm_railway_poi_point ->  layer_railway_poi:z10
+        SELECT *,
+              osm_id * 10 AS osm_id_hash
+        FROM osm_railway_poi_point
+        WHERE geometry && bbox
+          AND zoom_level = 10
+          AND class IN ('station', 'yard', 'junction', 'spur_junction', 'service_station', 'crossover', 'site', 'halt')
+
+        UNION ALL
+
+        -- etldoc: osm_railway_poi_point ->  layer_railway_poi:z11
+        -- etldoc: osm_railway_poi_point ->  layer_railway_poi:z12
+        SELECT *,
+              osm_id * 10 AS osm_id_hash
+        FROM osm_railway_poi_point
+        WHERE geometry && bbox
+          AND zoom_level BETWEEN 11 AND 12
+          AND class IN ('station', 'yard', 'junction', 'spur_junction', 'service_station', 'crossover', 'site', 'halt',
+                        'border', 'turntable', 'traverser', 'milestone')
+
+        UNION ALL
+
+        -- etldoc: osm_railway_poi_point ->  layer_railway_poi:z13
+        SELECT *,
+              osm_id * 10 AS osm_id_hash
+        FROM osm_railway_poi_point
+        WHERE geometry && bbox
+          AND zoom_level = 13
+          AND class IN ('station', 'yard', 'junction', 'spur_junction', 'service_station', 'crossover', 'site', 'halt',
+                        'border', 'turntable', 'traverser', 'milestone', 'owner_change', 'tram_stop', 'radio')
+
+        UNION ALL
+
+        -- etldoc: osm_railway_poi_point ->  layer_railway_poi:z14_
+        SELECT *,
+              osm_id * 10 AS osm_id_hash
+        FROM osm_railway_poi_point
+        WHERE geometry && bbox
+          AND zoom_level >= 14
+
+        UNION ALL
+
+        -- etldoc: osm_railway_poi_polygon ->  layer_railway_poi:z8
+        -- etldoc: osm_railway_poi_polygon ->  layer_railway_poi:z9
+        SELECT *,
+              CASE
+                  WHEN osm_id < 0 THEN -osm_id * 10 + 4
+                  ELSE osm_id * 10 + 1
+                  END AS osm_id_hash
+        FROM osm_railway_poi_polygon
+        WHERE geometry && bbox
+          AND zoom_level BETWEEN 8 AND 9
+          AND class IN ('station', 'yard', 'junction', 'spur_junction', 'service_station', 'crossover', 'site')
+
+        UNION ALL
+
+        -- etldoc: osm_railway_poi_polygon ->  layer_railway_poi:z10
+        SELECT *,
+              CASE
+                  WHEN osm_id < 0 THEN -osm_id * 10 + 4
+                  ELSE osm_id * 10 + 1
+                  END AS osm_id_hash
+        FROM osm_railway_poi_polygon
+        WHERE geometry && bbox
+          AND zoom_level = 10
+          AND class IN ('station', 'yard', 'junction', 'spur_junction', 'service_station', 'crossover', 'site', 'halt')
+
+        UNION ALL
+
+        -- etldoc: osm_railway_poi_polygon ->  layer_railway_poi:z11
+        -- etldoc: osm_railway_poi_polygon ->  layer_railway_poi:z12
+        SELECT *,
+              CASE
+                  WHEN osm_id < 0 THEN -osm_id * 10 + 4
+                  ELSE osm_id * 10 + 1
+                  END AS osm_id_hash
+        FROM osm_railway_poi_polygon
+        WHERE geometry && bbox
+          AND zoom_level BETWEEN 11 AND 12
+          AND class IN ('station', 'yard', 'junction', 'spur_junction', 'service_station', 'crossover', 'site', 'halt',
+                        'turntable', 'traverser')
+
+        UNION ALL
+
+        -- etldoc: osm_railway_poi_polygon ->  layer_railway_poi:z13
+        SELECT *,
+              CASE
+                  WHEN osm_id < 0 THEN -osm_id * 10 + 4
+                  ELSE osm_id * 10 + 1
+                  END AS osm_id_hash
+        FROM osm_railway_poi_polygon
+        WHERE geometry && bbox
+          AND zoom_level = 13
+          AND class IN ('station', 'yard', 'junction', 'spur_junction', 'service_station', 'crossover', 'site', 'halt',
+                        'turntable', 'traverser', 'tram_stop')
+
+        UNION ALL
+
+        -- etldoc: osm_railway_poi_polygon ->  layer_railway_poi:z14_
+        SELECT *,
+              CASE
+                  WHEN osm_id < 0 THEN -osm_id * 10 + 4
+                  ELSE osm_id * 10 + 1
+                  END AS osm_id_hash
+        FROM osm_railway_poi_polygon
+        WHERE geometry && bbox
+          AND zoom_level >= 14
+    ) AS railway_poi_union
 ORDER BY "rank"
 $$ LANGUAGE SQL STABLE
                 PARALLEL SAFE;
